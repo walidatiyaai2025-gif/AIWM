282716d3717e1a8e7fd3dee4205653e7293c9e12
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-04 16:47:56 +0300

    v1.4.0 fix smart sites experience compile imports

--- src/AIWordPressManager.Desktop/SitesHomeExperience.cs
+++ src/AIWordPressManager.Desktop/SitesHomeExperience.cs
@@ -1,6 +1,7 @@
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using AIWordPressManager.Desktop.ViewModels;
