af7b29295a66106d30cf2888e72f3d5db33858a0
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-04 21:31:50 +0300

    v1.8.0 Fix focus layout popup namespace

--- src/AIWordPressManager.Desktop/FocusLayoutExperience.cs
+++ src/AIWordPressManager.Desktop/FocusLayoutExperience.cs
@@ -1,6 +1,7 @@
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
